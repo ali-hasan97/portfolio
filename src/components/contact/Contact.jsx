095463b63612fcbbc8a20c4f983b69df8a41fe4f
ali-hasan97 <ali_hasan97@hotmail.com>
2022-09-15 04:57:52 -0500

    finished contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,19 @@
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_si7uxg8', 'template_7nyt7bu', form.current, 'x7jMb8OEbe_C8hpTA')
+
+    e.target.reset()
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -10,14 +22,14 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail />
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>hasan.syedali22@gmail.com</h5>
             <a href="mailto:hasan.syedali22@gmail.com">Send a message</a>
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required/>
           <input type="email" name='email' placeholder='Your Email' required/>
           <textarea name="message" id="" rows="7" placeholder='Your Message' required />
