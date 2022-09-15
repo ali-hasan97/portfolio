398876c0e63d1208adfab116d6a187bed53fcdeb
ali-hasan97 <ali_hasan97@hotmail.com>
2022-09-14 23:42:27 -0500

    finished about and navbar sections

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,51 @@
 import './about.css' 
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About me" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about_icon' />
+              <h5>Experience</h5>
+              <small>3+ years of experience</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className='about_icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className='about_icon' />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus vitae recusandae accusantium odit, blanditiis quae aspernatur doloremque consectetur tempore nulla quaerat nesciunt, consequatur eos aliquam quia. Sed fugit sequi nesciunt.
+          </p>
+          
+          <a href="#contact" className='btn btn-primary'>Let's talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
